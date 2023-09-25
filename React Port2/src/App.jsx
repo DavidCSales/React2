c5cd0347cb09b637891e8419f397857349ab30db
David Calábria Sales <108855817+DavidCSales@users.noreply.github.com>
2023-09-25 19:49:10 -0300

    scss moduladrizado
    Adicionado scss moduladrizado do header

--- React Port2/src/App.jsx
+++ React Port2/src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Header from './common/Header/Header'
 import Footer from './common/Footer/Footer'
-
+import './styles/app.scss'
 
 function App() {
   
--- React Port2/src/common/Header/Header.jsx
+++ React Port2/src/common/Header/Header.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import styles from "../../styles/components/Header/header.module.scss"
 
 export default function Header() {
     return(
         <>
-            <header>
+            <header className={`${styles["header-full-content"]}`}>
                 <h1>DavidCS</h1>
                 <nav>
                     <Link to={'/'} >Home</Link>
